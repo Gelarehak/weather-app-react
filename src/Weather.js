16af9dfbc88fefc46c10d08ae00cf0b091fdc455
Gelarehak <gelareh.akasheh@gmail.com>
2021-12-03 14:33:43 +0330

    icon

--- src/Weather.js
+++ src/Weather.js
@@ -24,6 +24,7 @@ export default function Weather() {
       </form>
       <div className="cityName">Tehran</div>
       <div className="date">Thursday 02 December 2021</div>
+      <div className="weatherType">Cloudy</div>
       <div>
         <img
           src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
@@ -32,104 +33,99 @@ export default function Weather() {
         />
       </div>
       <div className="temp">
-        <span className="degrees">15</span>
+        <span className="Degrees">15</span>
         <span className="cilsiuse">℃|℉</span>
       </div>
-      <div className="weatherType">Cloudy</div>
+
       <div className="row TodayImg">
-        <div className="col-1 Humidity">
-          <img
-            src="https://i.pinimg.com/564x/f7/99/af/f799afb17c962c62ce82d17140a89f74.jpg"
-            class="Water"
-            alt="dropwater"
-          />
+        <div className="col-3 Humidity ">
+          <i class="fas fa-tint Water"></i>
         </div>
-        <div className="col-2 maxmin">
-          <img
-            src="https://i.pinimg.com/564x/93/22/19/93221921ec3adc255fd79c09aa5982f3.jpg"
-            class="MaxMindegree"
-            alt="thermometer"
-          />
+        <div className="col-4 maxmin">
+          <i class="fas fa-thermometer-quarter MaxMindegree"></i>
         </div>
-        <div className="col-1 Wind">
-          <img
-            src="https://i.pinimg.com/564x/7f/38/48/7f3848314e386364581cd719ac2e0045.jpg"
-            class="windy"
-            alt="windimg"
-          />
+        <div className="col-3 Wind">
+          <i class="fas fa-wind windy"></i>
         </div>
       </div>
       <div className="row Today">
-        <div className="col-1 Humidity">
+        <div className="col-3 Humidity">
           <div className="Percentage">25%</div>
         </div>
-        <div className="col-2">
+        <div className="col-4">
           <div className="row MaxMin">
-            <div className="col-3 Max">18°</div>
+            <div className="col-2 Max">18°</div>
             <div className="col-5 Min">10°</div>
           </div>
         </div>
-        <div className="col-1 Wind">
+        <div className="col-3 Wind">
           <div className="Speed">
             <span className="Speedwind">12</span>
             <span className="KilometerH">km/h</span>
           </div>
         </div>
       </div>
+      <hr />
       <div className="row">
         <div className="col-2">
           {" "}
+          <div className="Day"> Sunday </div>
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="weatherpicture"
+            class="dayImg"
           />
-          <br />
-          25℃
+          <div className="DayDegree">25℃</div>
         </div>
         <div className="col-2">
           {" "}
+          <div className="Day"> Sunday </div>
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="weatherpicture"
+            class="dayImg"
           />
-          <br />
-          25℃
+          <div className="DayDegree">25℃</div>
         </div>
         <div className="col-2">
           {" "}
+          <div className="Day"> Sunday </div>
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="weatherpicture"
+            class="dayImg"
           />
-          <br />
-          25℃
+          <div className="DayDegree">25℃</div>
         </div>
         <div className="col-2">
           {" "}
+          <div className="Day"> Sunday </div>
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="weatherpicture"
+            class="dayImg"
           />
-          <br />
-          25℃
+          <div className="DayDegree">25℃</div>
         </div>
         <div className="col-2">
           {" "}
+          <div className="Day"> Sunday </div>
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="weatherpicture"
+            class="dayImg"
           />
-          <br />
-          25℃
+          <div className="DayDegree">25℃</div>
         </div>
         <div className="col-2">
           {" "}
+          <div className="Day"> Sunday </div>
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="weatherpicture"
+            class="dayImg"
           />
-          <br />
-          25℃
+          <div className="DayDegree">25℃</div>
         </div>
       </div>
     </div>
